aff59aacca014245bae442764f42b11d8d54fc78
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-11-17 18:27:20 +0530

    login credentital added

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -3,9 +3,13 @@ import {auth} from "../firbase"
 import {signInWithEmailAndPassword} from "firebase/auth";
 function Login(){
   
-  const [email , setEmail] = useState('')
-  const [password ,setPassword] = useState("")
-
+  let [email , setEmail] = useState('')
+  let [password ,setPassword] = useState("")
+  let [error , setError] = useState('');
+  let [loder , setLoder] = useState(false)
+  let [user , setUser] = useState(null);
+  
+  
   const trackEmail =(e) =>{
     setEmail(e.target.value)
   }
@@ -15,22 +19,46 @@ function Login(){
   }
 
   const printDetails =async ()=>{
-     alert(`${email} and ${password}`)
+    try {
+      
+    setLoder(true)
+    alert(`${email} and ${password}`)
     let userCred =await signInWithEmailAndPassword(auth, email, password)
-    console.log(userCred.user);
+    setUser(userCred.user)
+    // console.log(userCred.user);
+
+    } catch (error) {
+       setError(error.message)  
+       // after a little bit later error will false
+       setTimeout(() =>{
+        setError('')
+       } , 3000) 
+    }
+     setLoder(false)
+
+
   }
     return(
         <>
-          <h1> This is from Login</h1>
+        {
+          error !='' ? <h1>error is {error}</h1> 
+          :
+          loder ==true ? <h1>...loding</h1> 
+          :
+          user !=null ? <h1>user is{user.uid}</h1> 
+          : 
+ <>
+        <h1> This is from Login</h1>
       <input type="text" value={email} placeholder ="email"
-       onChange={trackEmail}
-      />
+       onChange={trackEmail}/>
       <br></br>
      <input type="password" name ="password" placeholder="password"
      onChange={trackPassowrd}
      />
      <br></br>
       <button type="submit" onClick={printDetails}>Log in</button>
+          </>
+        }
       </>
     )
 }
